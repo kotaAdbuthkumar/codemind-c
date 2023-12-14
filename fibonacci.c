d13004532f71293453dbeb2a71cefb357fa0910b
kotaAdbuthkumar <147487670+kotaAdbuthkumar@users.noreply.github.com>
2023-12-14 19:56:15 +0530

    #include <stdio.h> int main() {     int n, num1 = 0, num2 = 1, nextNum;     scanf("%d", &n);     for (int i = 1; i <= n; ++i) {         printf("%d ", num1);         nextNum = num1 + num2;         num1 = num2;         num2 = nextNum;     } }

---
+++ fibonacci.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n, num1 = 0, num2 = 1, nextNum;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+        printf("%d ", num1);
+        nextNum = num1 + num2;
+        num1 = num2;
+        num2 = nextNum;
+    }
+}
